044e0819be16fc6861976e2b8f7e6fb958a40cf3
taiga-mizushima <mizusima@nci.co.jp>
2023-02-12 10:33:19 +0900

    2nd commit.

--- controller/controller.py
+++ controller/controller.py
@@ -1,14 +1,6 @@
-from entity.entity import DataClassCard
 import repository.repository as Rep
 
 def execute():
-
-    # データ準備
-    data1 = DataClassCard("20230212", "AAA", "1", 100)
-    data2 = DataClassCard("20230212", "BBB", "0", 200)
-
-    Rep.insert(data1)
-    Rep.insert(data2)
 
     print("DataClassの内容を出力")
     for item in Rep.read():
--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 import controller.controller as Ctrl
+from entity.entity import DataClassCard
+import repository.repository as Rep
 
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
+
+    # データ準備
+    data1 = DataClassCard("20230212", "AAA", "1", 100)
+    data2 = DataClassCard("20230212", "BBB", "0", 200)
+
+    Rep.insert(data1)
+    Rep.insert(data2)
+
     Ctrl.execute()
